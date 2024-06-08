1d16b4254ea8393f42a9c181ea96c5417f57f9d2
atulverma0 <atul.verma@mindtickle.com>
2024-06-09 02:54:54 +0530

    update: init config

---
+++ main.py
@@ -0,0 +1,14 @@
+import discord
+import logging
+
+
+
+logging.basicConfig(
+    format="[%(asctime)s] [%(filename)s:%(lineno)d] %(message)s", level=logging.INFO
+)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
